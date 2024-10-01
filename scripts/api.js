8cfe3d41b6d118817841387293640f672605f2b9
LadyDefile <67084868+LadyDefile@users.noreply.github.com>
2024-10-01 11:53:31 -0500

    API tracker
    # Changes
    ## scripts\api.js
    * Moved tracker functions to `api.tracker`

--- scripts/api.js
+++ scripts/api.js
@@ -9,38 +9,40 @@ export class SimplerQuestsAPI {
         that GM-only functions are not exposed to players.
     */
     constructor() {
-        // Expands the tracker dock or opens the tracker window.
-        this.open = () => {
-            if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
-                // If the tracker is docked, uncollapse it.
-                UIManager.tracker.collapse(false);
-            } else {
-                UIManager.tracker.render(true, {
-                    focus: true,
-                });
-            }
-        };
-        // Minimizes the tracker dock or closes the tracker window.
-        this.close = () => {
-            if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
-                // If the tracker is docked, uncollapse it.
-                UIManager.tracker.collapse(true);
-            } else {
-                UIManager.tracker.close();
-            }
-        };
+        this.tracker = {
+            // Expands the tracker dock or opens the tracker window.
+            open: () => {
+                if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
+                    // If the tracker is docked, uncollapse it.
+                    UIManager.tracker.collapse(false);
+                } else {
+                    UIManager.tracker.render(true, {
+                        focus: true,
+                    });
+                }
+            },
+            // Minimizes the tracker dock or closes the tracker window.
+            close: () => {
+                if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
+                    // If the tracker is docked, uncollapse it.
+                    UIManager.tracker.collapse(true);
+                } else {
+                    UIManager.tracker.close();
+                }
+            },
 
-        // Toggles the state of the tracker dock or window.
-        this.toggle = () => {
-            if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
-                UIManager.tracker.collapse();
-                return UIManager.tracker.collapsed;
-            } else {
-                // If the state is greater than zero the window is
-                // rendering or rendered.
-                if (UIManager.tracker.rendered) UIManager.tracker.close();
-                else UIManager.tracker.render(true, { focus: true });
-            }
+            // Toggles the state of the tracker dock or window.
+            toggle: () => {
+                if (Settings.get(Settings.NAMES.TRACKER_DOCKED)) {
+                    UIManager.tracker.collapse();
+                    return UIManager.tracker.collapsed;
+                } else {
+                    // If the state is greater than zero the window is
+                    // rendering or rendered.
+                    if (UIManager.tracker.rendered) UIManager.tracker.close();
+                    else UIManager.tracker.render(true, { focus: true });
+                }
+            },
         };
 
         // Returns a filtered list of quests.
@@ -75,5 +77,7 @@ export class SimplerQuestsAPI {
             if (!id) return false;
             return QuestDatabase.removeQuest(id);
         };
+
+        this.updateQuest = (data) => {};
     }
 }
